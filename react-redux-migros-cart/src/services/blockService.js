d5d20d67c6705377892d4994c40a90c1c9c391ba
İpek Gözlek <ipekgozlek@gmail.com>
2026-01-25 15:59:01 +0300

    API URL guncellendi ve backend repo kaydi silindi

--- react-redux-migros-cart/src/services/blockService.js
+++ react-redux-migros-cart/src/services/blockService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:3001/blocks';
+const API_URL = 'https://migros-backend-p6a1.onrender.com';
 
-export const getBlocks=async () => {
-    const responce=await axios.get(API_URL);
-    return responce.data;       
+export const getBlocks = async () => {
+    const responce = await axios.get(`${API_URL}/blocks`);
+    return responce.data;
 };
 
 export const getBlockById = async (id) => {
-    const responce= await axios.get(`${API_URL}/${id}`);
+    const responce = await axios.get(`${API_URL}/blocks/${id}`);
     return responce.data;
 };
